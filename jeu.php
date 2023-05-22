ba4788ef963fc5ac1c3c614c16daf1e3349c5dc5
bilaly <billycsk@gmail.com>
2023-05-22 12:03:38 +0200

    Logique de jeu done

--- jeu.php
+++ jeu.php
@@ -1,13 +1,27 @@
 <?php
 $title = 'jeu';
+$myster = 200;
 include './layout/header.php';
 ?>
 
-<pre><?php var_dump($_GET) ?></pre>
-<form action="">
+{# <pre><?php var_dump($_GET) ?></pre> #}
+
+<?php
+$input = $_GET['number'];
+
+if ($input > $myster) : ?>
+    Le nombre saisie est trop grand
+<?php elseif ($input < $myster) : ?>
+    Le nombre saisie est trop petit
+<?php else : ?>
+    Bravo!!!!! vous avez deviné le bon nombre
+<?php endif ?>
+
+
+<form action="/jeu.php" method="GET">
     <label for="">Veuillez saisir le nombre</label>
-    <input type="number">
-    <button>Deviner</button>
+    <input type="number" name="number">
+    <button type="submit">Deviner</button>
 </form>
 
 
