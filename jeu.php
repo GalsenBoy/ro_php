91993e6afd46758e0e26899202be59336e8d2227
bilaly <billycsk@gmail.com>
2023-05-22 11:23:13 +0200

    Ajout de tailwind

--- jeu.php
+++ jeu.php
@@ -3,6 +3,13 @@ $title = 'jeu';
 include './layout/header.php';
 ?>
 
+<pre><?php var_dump($_GET) ?></pre>
+<form action="">
+    <label for="">Veuillez saisir le nombre</label>
+    <input type="number">
+    <button>Deviner</button>
+</form>
+
 
 
 <?php include './layout/footer.php'; ?>
