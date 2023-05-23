c4df6305d73024d3c4f450e7d795057ac2cbf2f8
bilaly <billycsk@gmail.com>
2023-05-23 12:36:19 +0200

    Ajout d'alerte après soumissions du formulaire

--- jeu.php
+++ jeu.php
@@ -9,13 +9,17 @@ include './layout/header.php';
 if (isset($_GET['number'])) :
     $input = $_GET['number'];
     if ($input > $myster) : ?>
-        <div>
-            <h3> Le nombre saisie est trop grand</h3>
+        <div class="alert alert-danger container mx 20">
+            <h3> Le nombre saisi est trop grand</h3>
         </div>
     <?php elseif ($input < $myster) : ?>
-
+        <div class="alert alert-danger container mx-20">
+            <h3> Le nombre saisi est trop petit</h3>
+        </div>
     <?php else : ?>
-        <h3> Bravo!!!!! vous avez deviné le bon nombre</h3>
+        <div class="alert alert-success container ">
+            <h3> Bravo!!! vous avez deviné le bon nombre</h3>
+        </div>
     <?php endif ?>
 <?php endif ?>
 
@@ -23,9 +27,11 @@ if (isset($_GET['number'])) :
 
 
 <form action="/jeu.php" method="GET">
-    <label for="">Veuillez saisir le nombre</label>
-    <input type="texte" class="peer h-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" placeholder=" " name="number">
-    <button type="submit" class="bg-blue-600 btn text-black hover:bg-sky-400">Deviner</button>
+    <div class="form-group">
+        <label for="">Veuillez saisir le nombre</label>
+        <input class="form-control" type="text placeholder=" name=" number">
+    </div>
+    <button type="submit" class="btn btn-primary">Deviner</button>
 </form>
 
 
