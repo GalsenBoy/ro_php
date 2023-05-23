5541771538129cac5840cfff8bee62909ac91c10
bilaly <billycsk@gmail.com>
2023-05-23 10:50:39 +0200

    Ajout de tailwind + revisite de la logique de code

--- jeu.php
+++ jeu.php
@@ -6,23 +6,26 @@ include './layout/header.php';
 
 
 <?php
-if (isset($_GET['number'])) {
+if (isset($_GET['number'])) :
     $input = $_GET['number'];
-}
+    if ($input > $myster) : ?>
+        <div>
+            <h3> Le nombre saisie est trop grand</h3>
+        </div>
+    <?php elseif ($input < $myster) : ?>
 
-if ($input > $myster) : ?>
-    Le nombre saisie est trop grand
-<?php elseif ($input < $myster) : ?>
-    Le nombre saisie est trop petit
-<?php else : ?>
-    Bravo!!!!! vous avez deviné le bon nombre
+    <?php else : ?>
+        <h3> Bravo!!!!! vous avez deviné le bon nombre</h3>
+    <?php endif ?>
 <?php endif ?>
+
+
 
 
 <form action="/jeu.php" method="GET">
     <label for="">Veuillez saisir le nombre</label>
-    <input type="number" name="number">
-    <button type="submit">Deviner</button>
+    <input type="texte" class="peer h-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" placeholder=" " name="number">
+    <button type="submit" class="bg-blue-600 btn text-black hover:bg-sky-400">Deviner</button>
 </form>
 
 
