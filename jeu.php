6cd091b9cec009dd6cf775660cf30b453181b9e3
bilaly <billycsk@gmail.com>
2023-05-24 10:14:39 +0200

    Ajout de formilaire pour passer une commande

--- jeu.php
+++ jeu.php
@@ -7,7 +7,7 @@ include './layout/header.php';
 
 <?php
 if (isset($_GET['number'])) :
-    $input = $_GET['number'];
+    $input = (int)$_GET['number'];
     if ($input > $myster) : ?>
         <div class="alert alert-danger container mx 20">
             <h3> Le nombre saisi est trop grand</h3>
@@ -27,11 +27,11 @@ if (isset($_GET['number'])) :
 
 
 <form action="/jeu.php" method="GET">
-    <div class="form-group">
+    <div class="form-group m-44 ">
         <label for="">Veuillez saisir le nombre</label>
-        <input class="form-control" type="text placeholder=" name=" number">
+        <input class="form-control " type="text placeholder=" name=" number">
     </div>
-    <button type="submit" class="btn btn-primary">Deviner</button>
+    <button type="submit" class="btn btn-primary m-2">Deviner</button>
 </form>
 
 
