4a2e22988e9ae7763d93b00e6544afd13820a831
bilaly <billycsk@gmail.com>
2023-05-24 10:23:42 +0200

    Ajout de style sur le formulaire avec bootstrap

--- commande.php
+++ commande.php
@@ -24,20 +24,27 @@ require './layout/header.php';
 <pre><?php var_dump($_GET) ?></pre>
 
 <form action="./commande.php" method="get">
-    <div>
-        <input type="checkbox" name="parfums[]" value="Fraise" id="">Fraise
-        <input type="checkbox" name="parfums[]" value="Chocolat" id="">Chocolat
-        <input type="checkbox" name="parfums[]" value="Vanille" id="">Vanille
+    <div class="form-group">
+        <input type="checkbox" class="form-check-input" name="parfums[]" value="Fraise" id="">
+        <label class="form-check-label" for="">Fraise</label>
+        <input type="checkbox" class="form-check-input" name="parfums[]" value="Chocolat" id="">
+        <label class="form-check-label" for="">Chocolat</label>
+        <input type="checkbox" class="form-check-input" name="parfums[]" value="Vanille" id="">
+        <label class="form-check-label" for="">Vanille</label>
     </div>
-    <div>
-        <input type="radio" name="cornets" value="Pot" id="">Pot
-        <input type="radio" name="cornets" value="Cornet" id="">Cornet
+    <div class="form-group">
+        <input type="radio" class="form-check-input" name="cornets" value="Pot" id="">
+        <label for="" class="form-check-label">Pot</label>
+        <input type="radio" class="form-check-input" name="cornets" value="Cornet" id="">
+        <label for="" class="form-check-label">Cornet</label>
     </div>
-    <div>
-        <input type="checkbox" value="Pépites de chocolat " name="supplements[]" id="">Pépites de chocolat
-        <input type="checkbox" name="supplements[]" value="Chantilly" id="">Chantilly
+    <div class="form-group">
+        <input type="checkbox" class="form-check-input" value="Pépites de chocolat " name="supplements[]" id="">
+        <label class="form-check-label" for="">Pépites de chocolat</label>
+        <input type="checkbox" class="form-check-input" name="supplements[]" value="Chantilly" id="">
+        <label class="form-check-label" for="">Chantilly</label>
     </div>
-    <button type="submit">Commander</button>
+    <button type="submit" class="btn btn-primary">Commander</button>
 </form>
 
 
