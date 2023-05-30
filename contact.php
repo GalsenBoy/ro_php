2e0e1d46bee52e04aa7dcc9fa135610ab8e61889
bilaly <billycsk@gmail.com>
2023-05-30 15:39:02 +0200

    Ajout des jours fermés

--- contact.php
+++ contact.php
@@ -5,12 +5,15 @@ require_once './config.php';
 ?>
 
 <h2>Les horaires d'ouverture</h2>
-<ul>
+<ul class="list-group">
     <?php foreach (JOURS as $index => $jour) : ?>
-        <?php if (isset(CRENEAUX[$index][0])) : ?>
-            <li><?= $jour ?> : <?= CRENEAUX[$index][0][0] ?> - <?= CRENEAUX[$index][0][1] ?>
+        <?php if (empty(CRENEAUX[$index][0]) && empty(CRENEAUX[$index][1])) : ?>
+            <li class="list-group-item"> <?= $jour ?> : fermé
+            <?php endif ?>
+            <?php if (isset(CRENEAUX[$index][0])) : ?>
+            <li class="list-group-item"><?= $jour ?> : <?= CRENEAUX[$index][0][0] ?>h - <?= CRENEAUX[$index][0][1] ?>h
                 <?php if (isset(CRENEAUX[$index][1])) : ?>
-                    <?= CRENEAUX[$index][1][0] ?> - <?= CRENEAUX[$index][1][1] ?>
+                    / <?= CRENEAUX[$index][1][0] ?>h - <?= CRENEAUX[$index][1][1] ?>h
             </li>
         <?php endif ?>
     <?php endif ?>
