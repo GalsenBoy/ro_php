debd80ada0b1c4124cb620e7eba6638e4241021a
bilaly <billycsk@gmail.com>
2023-05-30 11:14:59 +0200

    Ajout des créneaux

--- creneaux.php
+++ creneaux.php
@@ -1,1 +1,22 @@
-<h2>Hi!!</h2>
+<?php
+include './layout/header.php';
+require_once './config.php';
+$creneaux = [];
+
+foreach (CRENEAUX as $key) {
+    // var_dump($key);
+    $creneaux[] = $key;
+}
+?>
+
+
+<H1>Les horaires d'ouvertures</H1>
+<div>
+    <?php foreach ($creneaux as $creneau) : ?>
+        De <?= $creneau[0] ?>h à <?= $creneau[1] ?>h
+    <?php endforeach ?>
+</div>
+
+
+<?php
+include './layout/footer.php';
