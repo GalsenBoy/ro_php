82543ac03db8e73ad4a657ada2da4b70c26ac0e3
GalsenBoy <vipandco3@gmail.com>
2023-06-01 14:06:06 +0200

    Affichage du jour actuel avec un style particulier

--- contact.php
+++ contact.php
@@ -5,16 +5,26 @@ require_once './config.php';
 ?>
 
 <h2>Les horaires d'ouverture</h2>
-<ul>
+<ul class="list-group">
     <?php foreach (JOURS as $index => $jour) : ?>
+        <?php if(empty(CRENEAUX[$index][0]) && empty(CRENEAUX[$index][0])) :?>
+            <li class=" alert alert-danger"><?= $jour ?> : fermé</li>
+            <?php endif ?>
         <?php if (isset(CRENEAUX[$index][0])) : ?>
-            <li><?= $jour ?> : <?= CRENEAUX[$index][0][0] ?> - <?= CRENEAUX[$index][0][1] ?>
+            <?php if($index + 1 === (int)date('w')): ?>
+            <li class="alert alert-success"><?=$jour ?> : <?= CRENEAUX[$index][0][0] ?> - <?= CRENEAUX[$index][0][1] ?>
                 <?php if (isset(CRENEAUX[$index][1])) : ?>
-                    <?= CRENEAUX[$index][1][0] ?> - <?= CRENEAUX[$index][1][1] ?>
+                    / <?= CRENEAUX[$index][1][0] ?> - <?= CRENEAUX[$index][1][1] ?>
             </li>
+            <?php endif ?>
+            <?php else :?>
+                <li class="alert alert-danger"><?=$jour ?> : <?= CRENEAUX[$index][0][0] ?> - <?= CRENEAUX[$index][0][1] ?>
+                <?php if (isset(CRENEAUX[$index][1])) : ?>
+                    / <?= CRENEAUX[$index][1][0] ?> - <?= CRENEAUX[$index][1][1] ?>
+            </li>
+            <?php endif ?>
         <?php endif ?>
     <?php endif ?>
 <?php endforeach ?>
 </ul>
-
 <?php include './layout/footer.php';
