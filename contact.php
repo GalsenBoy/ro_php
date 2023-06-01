3a2191ee8180b611345521621728d5e34fd7778a
GalsenBoy <vipandco3@gmail.com>
2023-06-01 14:12:47 +0200

    Changement de style pour les non jj

--- contact.php
+++ contact.php
@@ -5,6 +5,9 @@ require_once './config.php';
 ?>
 
 <h2>Les horaires d'ouverture</h2>
+<div class="alert alert-success container mb-6">
+            <h3 class="text-center">Le magasin est ouvert</h3>
+        </div>
 <ul class="list-group">
     <?php foreach (JOURS as $index => $jour) : ?>
         <?php if(empty(CRENEAUX[$index][0]) && empty(CRENEAUX[$index][0])) :?>
@@ -18,7 +21,7 @@ require_once './config.php';
             </li>
             <?php endif ?>
             <?php else :?>
-                <li class="alert alert-danger"><?=$jour ?> : <?= CRENEAUX[$index][0][0] ?> - <?= CRENEAUX[$index][0][1] ?>
+                <li class="alert alert-light"><?=$jour ?> : <?= CRENEAUX[$index][0][0] ?> - <?= CRENEAUX[$index][0][1] ?>
                 <?php if (isset(CRENEAUX[$index][1])) : ?>
                     / <?= CRENEAUX[$index][1][0] ?> - <?= CRENEAUX[$index][1][1] ?>
             </li>
