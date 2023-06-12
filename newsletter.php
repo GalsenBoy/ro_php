341b843695fe0b7cfe2ff55ae809840d01b01afd
GalsenBoy <vipandco3@gmail.com>
2023-06-12 15:40:52 +0200

    Ajout du EOL + optimisation du code

--- newsletter.php
+++ newsletter.php
@@ -1,12 +1,11 @@
 <?php
 include './layout/header.php'; 
 $file = __DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'data.txt';
-echo file_get_contents($file);
 $email = $_POST['email'];
 if (filter_var($email, FILTER_VALIDATE_EMAIL)): ?>
     <div class="alert alert-success container">
     L'adresse email <?=$email ?> est considérée comme valide.
-    <?php file_put_contents($file,$email,FILE_APPEND); ?>
+    <?php file_put_contents($file,$email . PHP_EOL,FILE_APPEND); ?>
     </div>
     <?php else : ?>
         <div class="alert alert-danger container">
@@ -19,5 +18,4 @@ if (filter_var($email, FILTER_VALIDATE_EMAIL)): ?>
         <input type="text" name="email"  class="form-control" placeholder="Veuillez entrer votre email">
         <button type="submit" class="btn btn-primary mt-4">S'abonner</button>
     </div>
-    <h1><?= $file ?></h1>
 </form>
