a57433c247dffaade5c1ccd59ced424f5acec7b9
GalsenBoy <vipandco3@gmail.com>
2023-06-12 15:27:30 +0200

    Newsletter done (sans vérification si l'email existe)

--- newsletter.php
+++ newsletter.php
@@ -6,6 +6,7 @@ $email = $_POST['email'];
 if (filter_var($email, FILTER_VALIDATE_EMAIL)): ?>
     <div class="alert alert-success container">
     L'adresse email <?=$email ?> est considérée comme valide.
+    <?php file_put_contents($file,$email,FILE_APPEND); ?>
     </div>
     <?php else : ?>
         <div class="alert alert-danger container">
